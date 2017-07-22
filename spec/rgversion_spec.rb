947ba7ee032f0cb5f5eefa3764065c169514d114
Vladimir Avgustov <vavgustov@gmail.com>
2017-07-22 22:14:55 +0300

    Finally add some test coverage.

--- spec/rgversion_spec.rb
+++ spec/rgversion_spec.rb
@@ -1,7 +1,16 @@
-require "spec_helper"
+require 'spec_helper'
 
 RSpec.describe Rgversion do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Rgversion::VERSION).not_to be nil
+  end
+
+  describe 'spider' do
+    let(:spider) { Rgversion::Spider.new(%w[rgversion]) }
+
+    # this test will not work when version has been updated but not pushed to rubygems.org yet
+    it 'return correct version' do
+      expect(spider.output.first).to match('Rgversion::VERSION')
+    end
   end
 end
